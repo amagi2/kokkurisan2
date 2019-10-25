66ef74c38d68dd733ccc6cc4aecf3a83964bcfca
yuto otuka <akagekaga@gmail.com>
2019-10-25 15:59:19 +0900

    tesuto

--- Assets/Coin_Controller.cs
+++ Assets/Coin_Controller.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class Coin_Controller : MonoBehaviour
 {
-    private Vector3 pos;
-    private Vector3 WorldPointPos;
+    private Vector3 MousePos;//マウスの座標
+    private Vector3 WorldPointPos;//世界座標
+
+    //コイン移動
+    public void Move_Coin()
+    {
+        WorldPointPos.z = 0;
+        gameObject.transform.position = WorldPointPos;
+    }
+
+    //コイン定位置移動
+    public void Return_Coin()
+    {
+        gameObject.transform.position = new Vector3(0, 0, 0);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -16,18 +29,7 @@ public class Coin_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        pos = Input.mousePosition;
-        WorldPointPos = Camera.main.ScreenToWorldPoint(pos);
-        if (Input.GetMouseButton(0))
-        {
-            WorldPointPos.z = 0;
-            gameObject.transform.position = WorldPointPos;
-            Debug.Log("確認");
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            gameObject.transform.position = new Vector3(0, 0, 0);
-        }
-        
+        MousePos = Input.mousePosition;
+        WorldPointPos = Camera.main.ScreenToWorldPoint(MousePos);
     }
 }
--- Assets/Game_Controller.cs
+++ Assets/Game_Controller.cs
@@ -2,8 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class Game_Controller : MonoBehaviour
 {
+    GameObject Tap;
+    string Name;
+    public GameObject coin;
+
+    void Ray()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Tap = null;
+            Name = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2D = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+            if (hit2D)
+            {
+                Tap = hit2D.transform.gameObject;
+                Name = hit2D.transform.gameObject.name;
+            }
+        }
+        if (Input.GetMouseButton(0))
+        {
+            if (Name == "Coin")
+            {
+                coin.GetComponent<Coin_Controller>().Move_Coin();
+            }
+            Debug.Log(Tap);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            coin.GetComponent<Coin_Controller>().Return_Coin();
+        }
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +48,6 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Ray();
     }
 }
