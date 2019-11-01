c337b8efac18a9e1aa3eb2c879d71058f9aaa54a
yuto otuka <akagekaga@gmail.com>
2019-11-01 09:25:27 +0900

    コインコントローラーの追加 ボードのテスト 動画関連の実装（途中）

---
+++ Assets/Script/Coin_Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin_Controller : MonoBehaviour
+{
+    private Vector3 MousePos;//マウスの座標
+    private Vector3 WorldPointPos;//世界座標
+
+    //コイン移動
+    public void Move_Coin()
+    {
+        MousePos = Input.mousePosition;
+        WorldPointPos = Camera.main.ScreenToWorldPoint(MousePos);
+        WorldPointPos.z = 0;
+        gameObject.transform.position = WorldPointPos;
+    }
+
+    //コイン定位置移動
+    public void Return_Coin()
+    {
+        gameObject.transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
